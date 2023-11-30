e5e72673135fc8e85e2378304da93f5a64022f13
Andrey Rukin <rukin897@gmail.com>
2023-12-01 00:28:19 +0300

    feat: schemas

---
+++ tld2/schemas.py
@@ -0,0 +1,58 @@
+from pydantic import BaseModel
+from tld2.models import Status
+
+class User(BaseModel):
+    id: int
+    username: str
+    fullname: str
+    email: str
+    disabled: bool | None = None
+
+    class Config:
+        from_attributes = True
+
+
+class UserInDB(User):
+    hashed_password: str
+
+    class Config:
+        from_attributes = True
+
+class Library(BaseModel):
+    id: int
+    name: str
+    status: Status
+
+    class Config:
+        from_attributes = True
+
+
+class Company(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        from_attributes = True
+
+
+class Approver(BaseModel):
+    id: int
+    fullname: str
+    email: str
+    user_id: int
+    company_id: int | None
+    is_active: bool
+
+    class Config:
+        from_attributes = True
+
+
+class ApprovedLibrary(BaseModel):
+    id: int
+    name: str
+    approver_id: int
+    library_id: int
+
+
+    class Config:
+        from_attributes = True
