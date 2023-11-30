df0af3fde7d463139ac56ea9cb3bb297fb62d6b6
Andrey Rukin <rukin897@gmail.com>
2023-12-01 00:25:30 +0300

    feat: create db

---
+++ tld2/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from tld2.config import my_config
+
+
+engine = create_engine(my_config.db_url)
+db_session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = db_session()
+    try:
+        yield db
+    finally:
+        db.close()
