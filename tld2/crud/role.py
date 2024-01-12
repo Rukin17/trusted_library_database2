94046daf25a32b0a1cb7cd3502cf79cd2b34ff1d
Andrey Rukin <rukin899@gmail.com>
2024-01-12 21:03:55 +0300

    test commit

--- tld2/crud/role.py
+++ tld2/crud/role.py
@@ -14,6 +14,8 @@ def add_role_for_user(db: Session, user_id: int, role: RolesEnum) -> Role:
     return role_for_user
 
 
+
+
 def get_roles(db: Session, user_id: int) -> List[Role]:
     return db.query(Role).filter(Role.user_id == user_id).all()
 
